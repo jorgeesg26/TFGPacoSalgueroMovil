77c4e856071440674ea5804ef4d00de90d9562f7
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-06-08 11:20:58 +0200

    paco no sabe poner las variables bien, error del login arreglado

--- TFG_Movil/app/src/main/java/com/example/tfg_movil/model/authentication/classes/LoginResponse.kt
+++ TFG_Movil/app/src/main/java/com/example/tfg_movil/model/authentication/classes/LoginResponse.kt
@@ -3,6 +3,6 @@ package com.example.tfg_movil.model.authentication.classes
 import com.google.gson.annotations.SerializedName
 
 data class LoginResponse(
-    @SerializedName("StringToken") val accessToken: String,
-    @SerializedName("UserId") val userId: Int
+    @SerializedName("stringToken") val accessToken: String,
+    @SerializedName("userId") val userId: Int
 )
