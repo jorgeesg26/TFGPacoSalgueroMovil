b373d1686709a104d6d7a0f75fb6dd5a9b0dc73f
jorgeesg26 <jsalaba2610@g.educaand.es>
2025-06-08 12:37:45 +0200

    registro funcional tambien

--- TFG_Movil/app/src/main/java/com/example/tfg_movil/views/Register.kt
+++ TFG_Movil/app/src/main/java/com/example/tfg_movil/views/Register.kt
@@ -1,37 +1,23 @@
 package com.example.tfg_movil.views
 
+import android.net.Uri
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.tfg_movil.R
 import com.example.tfg_movil.model.authentication.classes.AuthState
 import com.example.tfg_movil.model.navigation.RutasNavegacion
@@ -42,12 +28,16 @@ fun Register(viewModelAuth: ViewModelAuth, navController: NavController) {
     val context = LocalContext.current
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var user_id by remember { mutableStateOf("") }
     var passwordVisible by remember { mutableStateOf(false) }
-    val authState by viewModelAuth.authState.collectAsState()
     var nickname by remember { mutableStateOf("") }
     var confirmPassword by remember { mutableStateOf("") }
-    var profilePhotoUri by remember { mutableStateOf<android.net.Uri?>(null) }
+    var profilePhotoUri by remember { mutableStateOf<Uri?>(null) }
+    val authState by viewModelAuth.authState.collectAsState()
+
+    // Launcher para seleccionar la imagen
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        profilePhotoUri = uri
+    }
 
     LaunchedEffect(authState) {
         when (authState) {
@@ -60,9 +50,8 @@ fun Register(viewModelAuth: ViewModelAuth, navController: NavController) {
         }
     }
 
-
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -72,18 +61,15 @@ fun Register(viewModelAuth: ViewModelAuth, navController: NavController) {
         )
 
         Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(
             value = nickname,
             onValueChange = { nickname = it },
             label = { Text("Nickname") }
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = confirmPassword,
-            onValueChange = { confirmPassword = it },
-            label = { Text("Confirm Password") },
-            visualTransformation = PasswordVisualTransformation()
-        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         OutlinedTextField(
             value = email,
             onValueChange = { email = it },
@@ -111,18 +97,39 @@ fun Register(viewModelAuth: ViewModelAuth, navController: NavController) {
             }
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = { launcher.launch("image/*") },
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondaryContainer)
+        ) {
+            Text("Seleccionar foto de perfil")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
 
         Button(
             onClick = {
-                profilePhotoUri?.let { uri ->
+                if (profilePhotoUri != null) {
                     viewModelAuth.signUp(
                         nickname,
                         email,
                         password,
                         confirmPassword,
-                        uri,
-                        context.contentResolver                     )
+                        profilePhotoUri!!,
+                        context.contentResolver
+                    )
+                } else {
+                    Toast.makeText(context, "Selecciona una foto de perfil", Toast.LENGTH_SHORT).show()
                 }
             },
             enabled = authState != AuthState.Loading,
